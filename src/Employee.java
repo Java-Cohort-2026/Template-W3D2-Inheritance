82531f57d9f1ad2cdab67d05285f1f5162d2f37e
mohitmhamunkar <74923844+mohitmhamunkar@users.noreply.github.com>
2026-05-11 11:36:01 +0530

    Create Employee.java

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    // TODO: Create two PRIVATE attributes:
+    // 1. String name
+    // 2. double salary
+
+    // TODO: Create a Constructor that takes a name and a salary.
+
+    // TODO: Create public Getters for both name and salary.
+}
