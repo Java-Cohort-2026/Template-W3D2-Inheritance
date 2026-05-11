0eb9bee4b4b91fcba593709dd7f569a7f3c4ee5a
mohitmhamunkar <74923844+mohitmhamunkar@users.noreply.github.com>
2026-05-11 11:36:19 +0530

    Create Manager.java

---
+++ src/Manager.java
@@ -0,0 +1,10 @@
+// TODO: Use the 'extends' keyword so Manager inherits from Employee
+public class Manager {
+    // TODO: Create a PRIVATE attribute: double bonus
+
+    // TODO: Create a Constructor that takes name, salary, and bonus.
+    // Use super() to pass name and salary to the Employee class!
+
+    // TODO: Create a public method called 'calculateTotalCompensation'
+    // It should return the sum of their salary (use getSalary()!) and their bonus.
+}
